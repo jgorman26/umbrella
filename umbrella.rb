95f8e0567fa117110831dfa38cb3c93cbd2d2d58
jgorman26 <156133160+jgorman26@users.noreply.github.com>
2025-01-28 05:07:05 +0000

    Check of initial call for location

--- umbrella.rb
+++ umbrella.rb
@@ -4,4 +4,14 @@ require "dotenv/load"
 
 # Write your soltuion here!
 
-pp ENV.fetch("PIRATE_WEATHER_KEY")
+google_maps_key = ENV.fetch("GMAPS_KEY")
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+puts "========================================"
+puts "Will you need an umbrella today?"
+puts "========================================"
+puts "Where are you?"
+user_location = gets.chomp
+puts "Checking #{user_location}'s weather..."
+
+google_maps_url = "https://www.google.com/maps/" + 
